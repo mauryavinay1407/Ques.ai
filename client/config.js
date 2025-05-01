4f1933f46dbe667926fbea6c16d2e231d395771f
mauryavinay1407 <vinaymaurya1407@gmail.com>
2025-05-01 21:05:12 +0530

    fix: favicon updated

--- client/config.js
+++ client/config.js
@@ -1,3 +1,3 @@
 export const BASE_URL = import.meta.env.DEV
-? "http://localhost:4000"  
-: "https://ques-ai-backend-hkgk.onrender.com"
+    ? "http://localhost:4000"
+    : "https://ques-ai-backend-hkgk.onrender.com";
